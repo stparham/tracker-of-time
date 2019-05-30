c66973b629aa4acf3e383c9a7a27de974f4e8f6e
Stanton Parham <stanton8parham8@gmail.com>
2019-05-30 16:25:06 -0400

    Moved all content other than the header into a new file in preparation for other screens

--- src/App.js
+++ src/App.js
@@ -1,93 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import ActivityList from './ActivityList';
-
-import { fuzzySearch } from './fuzzySearchUtils';
-
-const ACTIVITIES = [
-  {
-    id: 0,
-    title: 'Entertainment',
-  },
-  {
-    id: 1,
-    title: 'Work',
-  },
-  {
-    id: 2,
-    title: 'Eating',
-  },
-  {
-    id: 3,
-    title: 'Sleeping',
-  },
-  {
-    id: 4,
-    title: 'Blah',
-  },
-  {
-    id: 5,
-    title: 'Entertainment',
-  },
-  {
-    id: 6,
-    title: 'Work',
-  },
-  {
-    id: 7,
-    title: 'Eating',
-  },
-  {
-    id: 8,
-    title: 'Sleeping',
-  },
-  {
-    id: 9,
-    title: 'Blah',
-  },
-];
+import StartActivity from './StartActivity';
 
 function App() {
-  const [allActivities] = useState(ACTIVITIES);
-  const [visibleActivities, setVisibleActivities] = useState(allActivities);
-  const [selectedActivity, setSelectedActivity] = useState(null);
-
-  function filterResults(event) {
-    const inputText = event.target.value;
-    const filteredActivities = fuzzySearch(inputText, allActivities);
-    setVisibleActivities(filteredActivities);
-  }
-
-  function selectActivity(id) {
-    // if the currently selected id matches the passed in one, then deselect the activity
-    // otherwise select the passed in one
-    setSelectedActivity(selectedActivity === id ? null : id);
-  }
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Tracker of Time</h1>
       </header>
 
-      <input type="text" placeholder="Search for an activity..." onChange={filterResults}></input>
-
-      <br/>
-
-      <ActivityList
-        activities={visibleActivities}
-        selectActivity={selectActivity}
-        selectedActivity={selectedActivity}
-      />
-
-      <br/>
-
-      <button onClick={() => console.log('Creating new activity...')}>New Activity</button>
-
-      <br/>
-
-      <button onClick={() => console.log('Starting activity...')}>Start</button>
+      <StartActivity />
     </div>
   );
 }
---
+++ src/StartActivity.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import './StartActivity.css';
+
+import ActivityList from './ActivityList';
+
+import { fuzzySearch } from './fuzzySearchUtils';
+
+const ACTIVITIES = [
+  {
+    id: 0,
+    title: 'Entertainment',
+  },
+  {
+    id: 1,
+    title: 'Work',
+  },
+  {
+    id: 2,
+    title: 'Eating',
+  },
+  {
+    id: 3,
+    title: 'Sleeping',
+  },
+  {
+    id: 4,
+    title: 'Blah',
+  },
+  {
+    id: 5,
+    title: 'Entertainment',
+  },
+  {
+    id: 6,
+    title: 'Work',
+  },
+  {
+    id: 7,
+    title: 'Eating',
+  },
+  {
+    id: 8,
+    title: 'Sleeping',
+  },
+  {
+    id: 9,
+    title: 'Blah',
+  },
+];
+
+function StartActivity() {
+  const [allActivities] = useState(ACTIVITIES);
+  const [visibleActivities, setVisibleActivities] = useState(allActivities);
+  const [selectedActivity, setSelectedActivity] = useState(null);
+
+  function filterResults(event) {
+    const inputText = event.target.value;
+    const filteredActivities = fuzzySearch(inputText, allActivities);
+    setVisibleActivities(filteredActivities);
+  }
+
+  function selectActivity(id) {
+    // if the currently selected id matches the passed in one, then deselect the activity
+    // otherwise select the passed in one
+    setSelectedActivity(selectedActivity === id ? null : id);
+  }
+
+  return (
+    <div className="StartActivity">
+      <input type="text" placeholder="Search for an activity..." onChange={filterResults}></input>
+
+      <br/>
+
+      <ActivityList
+        activities={visibleActivities}
+        selectActivity={selectActivity}
+        selectedActivity={selectedActivity}
+      />
+
+      <br/>
+
+      <button onClick={() => console.log('Creating new activity...')}>New Activity</button>
+
+      <br/>
+
+      <button onClick={() => console.log('Starting activity...')}>Start</button>
+    </div>
+  );
+}
+
+export default StartActivity;
