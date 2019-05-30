380e93c36b5aba31a472c08474998563fe84b03d
Stanton Parham <stanton8parham8@gmail.com>
2019-05-29 23:33:44 -0400

    Made a mock up the first screen; implemented a ruidmentary fuzzy search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const ACTIVITIES = [
+  'Entertainment',
+  'Work',
+  'Eating',
+  'Sleeping',
+  'Blah',
+  'Entertainment',
+  'Work',
+  'Eating',
+  'Sleeping',
+  'Blah',
+];
+
+
+/**
+ * Returns whether or not the short string is a fuzzy match
+ * with the long string.
+ *
+ * A word fuzzy matches with another word if all of the
+ * letters in the word appear in the same order in the other word.
+ *
+ * @param {string} shortStr the short string
+ * @param {string} longStr the long string
+ * @return {boolean} whether or not the short string is a
+ *    fuzzy match with the long string
+ */
+function fuzzyMatch(shortStr, longStr) {
+  let n = -1;
+  for (let i = 0; i < shortStr.length; i++) {
+    const curLetter = shortStr[i];
+    n = longStr.toLowerCase().indexOf(curLetter, n + 1);
+    if (n === -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ * Returns an array of the strings from the given strings to search that
+ * fuzzy match with the given comparison string.
+ *
+ * @param {string} comparisonString the string to match
+ * @param {array} stringsToSearch the strings to match with the comparisonString
+ * @return {array} a subset of the stringsToSearch that match the comparisonString
+ */
+function fuzzySearch(comparisonString, stringsToSearch) {
+  const lowerCompStr = comparisonString.toLowerCase();
+  return stringsToSearch.filter((str) => fuzzyMatch(lowerCompStr, str));
+}
+
 function App() {
+  const [allActivities] = useState(ACTIVITIES);
+  const [visibleActivities, setVisibleActivities] = useState(allActivities);
+
+  function filterResults(event) {
+    const inputText = event.target.value;
+    const filteredActivities = fuzzySearch(inputText, allActivities);
+    setVisibleActivities(filteredActivities);
+  }
+
+  let listItems = <li key="DNE">No matches found</li>;
+  if (visibleActivities.length > 0) {
+    listItems = visibleActivities.map((activity, idx) => {
+      return <li key={idx}>{activity}</li>;
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Tracker of Time</h1>
       </header>
+
+      <input type="text" placeholder="Search for an activity..." onChange={filterResults}></input>
+
+      <br/>
+
+      <div className="App-center-container">
+        <ul className="App-activity-list">
+          { listItems }
+        </ul>
+      </div>
+
+      <br/>
+
+      <button onClick={() => console.log('Creating new activity...')}>New Activity</button>
+
+      <br/>
+
+      <button onClick={() => console.log('Starting activity...')}>Start</button>
     </div>
   );
 }
